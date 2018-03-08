0b8347ce27cb9bd93de30d8e3a797c844380aed7
Tuyen Dang <tuyendq@gmail.com>
2018-03-08 22:55:19 +0700

    add classes

---
+++ classes/BankAccount.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+    }
+
+
+}
---
+++ classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Tuyen", 100000000000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner}.");
+        }
+    }
+}
