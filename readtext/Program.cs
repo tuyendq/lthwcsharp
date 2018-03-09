bb0ffaf21626f9dc1cfbbfe9db328a84b140b2b1
Tuyen Dang <tuyendq@gmail.com>
2018-03-09 16:15:54 +0700

    readtext: read whole file -> to fast to read

---
+++ readtext/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace readtext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // Read "sampleQuotes.txt" file
+            string filename = "sampleQuotes.txt";
+            var lines = ReadFrom(filename);
+            //var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+        // ReadFrom method
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+    }
+}
