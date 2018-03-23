f8404745db8ab3d42c2de67f6ba8d90b5c9e5d03
Tuyen Dang <tuyendq@gmail.com>
2018-03-23 19:23:32 +0700

    add stringbuilder

---
+++ stringbuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿/* C# Convention
+* To append strings in loops, especially when you are working with
+* large amount of text, use a StringBuilder object.
+*/
+using System;
+using System.Text;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase = "lalalalalalalalalalalalalalalalalalalalalalalalalalalalalala";
+            var manyPhrases = new StringBuilder();
+            for (var i = 0; i < 10000; i++)
+            {
+                manyPhrases.Append(phrase);
+            }
+            Console.WriteLine("tra" + manyPhrases);
+        }
+    }
+}
