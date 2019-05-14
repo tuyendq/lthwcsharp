eadbcf0dd09578d5fc1fdf5f4caace4feac69f92
Tuyen Dang <tuyendq@gmail.com>
2019-05-14 21:34:00 +0700

    add attributes folder

---
+++ attributes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ObsoleteClass A = new ObsoleteClass();
+        }
+    }
+
+    public class MySpecialAttribute : Attribute
+    {
+
+    }
+
+    [MySpecial]
+    public class SomeOtherClass
+    {
+
+    }
+
+    //[Obsolete("This class is obsolete. Please user ... class")]
+    public class ObsoleteClass
+    {
+        public ObsoleteClass()
+        {
+            Console.WriteLine("This class is obsolete!");
+        }
+
+        ~ObsoleteClass()
+        {
+            Console.WriteLine("Object is destroyed!");
+        }
+       
+    }
+}
