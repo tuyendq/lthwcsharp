1a4e3a09d19b1e75144e1f9677848fec90c7d82f
Tuyen Dang <tuyendq@gmail.com>
2023-11-05 12:37:55 +0700

    add SavingAcct.cs class

--- Class/Modifiers/Program.cs
+++ Class/Modifiers/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ModifiersDemo
-{
+﻿namespace ModifiersDemo;
+
     class Program
     {
         static void Main() {
+
+            // Play with Account class
             Account account1 = new Account("Tuyen", "Dang");
             System.Console.WriteLine(account1.GetOwner());
+
+            // System.Console.WriteLine(account1);  // Should return Namespace.Classname
+
         }
     }
-}
+
---
+++ Class/Modifiers/SavingAcct.cs
@@ -0,0 +1,9 @@
+namespace ModifiersDemo;
+
+public class SavingAcct : Account
+{
+    public SavingAcct(string firstName, string lastName, decimal balance) : base(firstName, lastName, balance)
+    {
+        
+    }
+}
